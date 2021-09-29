fa54b2693fb59856d643103751856a3feeb2c0fc
jonesramos01 <jonjones.rj@gmail.com>
2021-09-28 21:14:39 -0300

    commit

---
+++ MÓDULO A: CONHECENDO O JS/1.primeiro-script/script.js
@@ -0,0 +1,15 @@
+/*::::MÓDULO A: CONHECENDO O JS::::*/
+/*1.PRIMEIRO SCRIPT*/
+
+
+//alert
+
+alert("Olá, seja bem vindo!");
+
+//confirm
+
+confirm("Está gostando do conteúdo?")
+
+//prompt
+
+prompt("Qual o seu nome?");
