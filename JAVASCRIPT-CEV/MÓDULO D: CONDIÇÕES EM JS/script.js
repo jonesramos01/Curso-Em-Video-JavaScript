f575e753b3af4bc61a6258e8d5f61872d7aad2fc
jonesramos01 <jonjones.rj@gmail.com>
2021-10-05 15:25:11 -0300

    commit

---
+++ JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/script.js
@@ -0,0 +1,32 @@
+/*MÓDULO D: CONDICIONAIS*/
+//1.CONDICIONAIS SIMPLES
+//2.CONDICIONAIS COMPOSTAS
+//3.CONDICIONAIS ANINHADAS
+//
+
+//1.CONDIÇÃO SIMPLES:
+
+//Ex1: LIMITE DE VELOCIDADE
+
+/*
+var vel = 102;
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+    if(vel > 60){
+        console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+    }
+console.log(`Dirija sempre usando cinto de segurança!`);
+*/
+
+//2.CONDIÇÃO COMPOSTA:
+
+//Ex: 
+/*
+var país = "Brasil, BRA"
+console.log(`Nascido em: ${país}`)
+if(país == "Brasil"){
+    console.log("Você é: Brasileiro");
+} else {
+    console.log("Você é: Estrangeiro");
+}
+*/
+
