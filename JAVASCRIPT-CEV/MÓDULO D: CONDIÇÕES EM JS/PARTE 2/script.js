1fd8ecd9e6466f88e850a9f556b75cbceefba532
jonesramos01 <jonjones.rj@gmail.com>
2021-10-06 22:00:29 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/PARTE 2/script.js
+++ JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/PARTE 2/script.js
@@ -6,6 +6,8 @@
 //····································································································
 //::::3.CONDICIONAIS ANINHADAS:::::
 //····································································································
+//if é recomendado para testar intervalos diversos!
+
 //EX TESTE NO NODE.JS: "OBS: EXTENSÃO NODE EXEC INSTALADA. HABILITA O F8 PARA EXECUTAR O NODE.JS."
 //····································································································
 
@@ -88,3 +90,72 @@ if(hora >= 6 && hora < 12){
 //····································································································
 //::::4.CONDICIONAIS MÚLTIPLAS::::
 //····································································································
+//switch é recomendado para testar dados, valores pontuais
+
+/*
+//EX:
+switch(expressão){
+    case valor 1:
+
+        break
+    case valor 2:
+
+        break
+    case valor 3:
+
+        break
+    default:
+
+        break
+}
+*/
+
+/**/
+//
+var diaSem = 7;
+//var agora = new Date();
+//var diaSem = agora.getDay();
+
+/*
+    0 = Domingo
+    1 = Segunda
+    2 = Terça
+    3 = Quarta
+    4 = Quinta
+    5 = Sexta
+    6 = Sábado
+*/
+
+//console.log(diaSem);
+switch(diaSem){
+    case 0:
+        console.log(`Domingo`);
+        break;
+
+        case 1:
+            console.log(`Segunda`);
+            break;
+
+            case 2:
+                console.log(`Terça`);
+                break;
+
+                case 3:
+                    console.log(`Quarta`);
+                    break;
+
+                    case 4:
+                        console.log(`Quinta`);
+                        break;
+
+                        case 5:
+                            console.log(`Sexta`);
+                            break;
+
+                            case 6:
+                                console.log(`Sábado`);
+                                break;
+
+                                default:
+                                    console.log(`[ERRO] Dia inválido`);
+}                               
