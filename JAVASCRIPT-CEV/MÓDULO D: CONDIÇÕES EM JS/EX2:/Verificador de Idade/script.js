4e8400d1830038c1eda1170e36d879bdc404e1fc
Jones Ramos <jonjones.rj@gmail.com>
2021-10-08 15:38:35 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/EX2:/Verificador de Idade/script.js
+++ JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/EX2:/Verificador de Idade/script.js
@@ -1,1 +1,12 @@
 /*VERIFICADOR DE IDADE*/
+//Formulário:
+//Botão do Formulário:
+function verificar(){
+    /*alert('Funcionou!'); //Teste */
+    var dataAtual = new Date(); //Data atual
+    var anoAtual = data.getFullYear(); //Ano atual
+    var anoForm = document.getElementById(`txtano`);
+    var res = document.getElementById(`res`) 
+    //Ou 
+    //var res = document.querySelector(`div#res`);
+}
