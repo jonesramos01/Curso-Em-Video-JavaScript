fa3097b10bc29a7d6709c3cd2988fd5bd1b7afec
jonesramos01 <jonjones.rj@gmail.com>
2021-10-10 18:01:12 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/EX2:/Verificador de Idade/script.js
+++ JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/EX2:/Verificador de Idade/script.js
@@ -3,10 +3,21 @@
 //Botão do Formulário:
 function verificar(){
     /*alert('Funcionou!'); //Teste */
-    var dataAtual = new Date(); //Data atual
-    var anoAtual = data.getFullYear(); //Ano atual
-    var anoForm = document.getElementById(`txtano`);
-    var res = document.getElementById(`res`) 
+
+    var data = new Date(); //Data atual (automático - dados do sistema)
+    var ano = data.getFullYear(); //Ano atual (automático - dados do sistema)
+
+    var fano = document.getElementById("txtano"); //Ano do formulário
+    var res = document.getElementById("res"); //Resposta do formulário
     //Ou 
     //var res = document.querySelector(`div#res`);
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert("[ERRO] Verifique os dados e tente novamente!");
+    } else {
+        /*window.alert("Tudo ok!"); //Teste */
+        var fsex = document.getElementsByName("radsex");
+        var idade = ano - Number(fano.value);
+        res.innerHTML = `Idade calculada: ${idade}`
+    }
 }
