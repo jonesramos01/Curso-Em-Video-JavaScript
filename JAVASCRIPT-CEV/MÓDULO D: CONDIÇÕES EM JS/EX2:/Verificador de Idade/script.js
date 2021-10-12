49632c2312f0a56d3d4ff515fed7f37666691d5d
jonesramos01 <jonjones.rj@gmail.com>
2021-10-12 13:59:16 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/EX2:/Verificador de Idade/script.js
+++ JAVASCRIPT-CEV/MÓDULO D: CONDIÇÕES EM JS/EX2:/Verificador de Idade/script.js
@@ -14,15 +14,44 @@ function verificar(){
 
     if(fano.value.length == 0 || Number(fano.value) > ano){
         window.alert("[ERRO] Verifique os dados e tente novamente!");
-    } else {
+    } else{
         /*window.alert("Tudo ok!"); //Teste */
         var fsex = document.getElementsByName("radsex");
         var idade = ano - Number(fano.value);
         /*res.innerHTML = `Idade calculada: ${idade}` //Teste */ 
+        
         var genero = ""
 
+        var img = document.createElement("img"); //
+        img.setAttribute("id", "foto"); //
+
         if(fsex[0].checked){
-            genero = ""
-        }
+            genero = "Homem";
+            
+            if(idade >= 0 && idade <10){
+                //criança
+            } else if(idade >= 10 && idade < 21){
+                //jovem
+            } else if(idade >= 21 && idade < 50){
+                //Adulto
+            } else if(idade >= 50){
+                //idoso
+            }
+
+        } else if(fsex[1].checked){
+            genero = "Mulher";
+
+            if(idade >= 0 && idade <10){
+                //criança
+            } else if(idade >= 10 && idade < 21){
+                //jovem
+            } else if(idade >= 21 && idade < 50){
+                //Adulto
+            } else if(idade >= 50){
+                //idoso
+            }
+        }  
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+        
     }
 }
