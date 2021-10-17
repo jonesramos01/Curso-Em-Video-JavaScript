a1b028964f731036396599acc1a8628f44a9cec0
jonesramos01 <jonjones.rj@gmail.com>
2021-10-17 07:04:54 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO E: REPETIÇÕES EM JS/EX1: VAMOS CONTAR/script.js
+++ JAVASCRIPT-CEV/MÓDULO E: REPETIÇÕES EM JS/EX1: VAMOS CONTAR/script.js
@@ -5,7 +5,8 @@ function contar(){
     var res = document.getElementById("res");
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert("[ERRO] Faltam dados!");
+        window.alert("[ERRO] Impossível contar!");
+
     } else {
         //alert("Tudo ok!"); //Teste
         res.innerHTML = "Contando..."
@@ -13,7 +14,27 @@ function contar(){
         var i = Number(ini.value);
         var f = Number(fim.value);
         var p = Number(passo.value);
+
+        if(p <= 0){
+            window.alert("[Erro] Passo inválido! Será considerado passo de valor 1")
+            p = 1
+        }
         
+        if(i < f){
+            //Contagem crescente:
+            for(var c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449} ` //Unicode: U+1F600; No JS: \u{1F449}
+            }
+            //res.innerHTML += `\u{1F3C1}`     
+       
+        } else {
+           //Contagem regressiva:
+           for(var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449} `
+           } 
+           //res.innerHTML += `\u{1F3C1}`
+       }
+       res.innerHTML += `\u{1F3C1}`
     }
 
 }
