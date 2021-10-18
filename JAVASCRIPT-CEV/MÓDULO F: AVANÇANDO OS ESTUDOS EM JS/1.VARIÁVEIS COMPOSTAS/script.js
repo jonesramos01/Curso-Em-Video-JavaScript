dc18f68e770021ec899d637fcd5e88a3c87566ef
jonesramos01 <jonjones.rj@gmail.com>
2021-10-18 10:16:34 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/1.VARIÁVEIS COMPOSTAS/script.js
+++ JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/1.VARIÁVEIS COMPOSTAS/script.js
@@ -3,3 +3,27 @@
 /*2.FUNÇÕES*/
 /*EX:*/
 /*4.PRÓXIMOS PASSOS*/
+//······································································································
+/*1.VARIÁVEIS COMPOSTAS:*/
+/*
+var num = []
+//console.log(num) //Ou
+console.log(`Nosso vetor é o ${num}`)
+*/
+
+/*
+var num = [1, 3, 5]
+//console.log(num) //Ou
+console.log(`Nosso vetor é o ${num}`)
+*/
+
+/*
+var num = [1, 3, 5]
+//console.log(num) //Ou
+//console.log(`Nosso vetor é o ${num}`)
+
+//console.log(`O vetor tem ${num.length} posições`)
+*/
+
+
+
