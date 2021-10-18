5dd804325f0065285b6deff29294e12710e13431
jonesramos01 <jonjones.rj@gmail.com>
2021-10-18 16:58:19 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/1.VARIÁVEIS COMPOSTAS/script.js
+++ JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/1.VARIÁVEIS COMPOSTAS/script.js
@@ -5,6 +5,23 @@
 /*4.PRÓXIMOS PASSOS*/
 //······································································································
 /*1.VARIÁVEIS COMPOSTAS:*/
+
+/*
+var a = [a0, a1, a2, ... ]
+        
+a = Array/Vetor
+0, 1, 2, ... = Índice/Chave
+[a0, a1, a2, ... ] = Elemento
+a0 = Conteúdo/Valor 
+
+//EX:
+var a = [1, 2, 3, 4]
+a[4] = 5 //Adiciona o valor 5 a posição 4 no vetor. 
+a.push(y) //Adiciona o valor 5 a última posição, seja ela qual for.
+a.length //Para saber o comprimento do Array. "Quantidade de elementos/posições". Obs: No JS é sem ()
+num.sort() //Ordena os elementos em ordem crescente (caso realmente precise).
+*/
+
 /*
 var num = []
 //console.log(num) //Ou
@@ -12,18 +29,25 @@ console.log(`Nosso vetor é o ${num}`)
 */
 
 /*
-var num = [1, 3, 5]
-//console.log(num) //Ou
-console.log(`Nosso vetor é o ${num}`)
+//EX1:
+var num = [1, 3, 5, 7]
+console.log(num) //Mostra o array/vetor.
+console.log(num.length) //Mostra o comprimento do array/vetor. "Neste possui 4 posições/elementos".
+num[4] = 9 //Adiciona o valor 9 na posição 4 do array/vetor.
+console.log(num)
+console.log(num.length)
 */
 
 /*
-var num = [1, 3, 5]
-//console.log(num) //Ou
-//console.log(`Nosso vetor é o ${num}`)
-
-//console.log(`O vetor tem ${num.length} posições`)
+EX2:
+var num = [7, 5, 3, 1]
+console.log(num) //Mostra o array/vetor.
+console.log(num.length) //Mostra o comprimento do array/vetor.
+num[4] = 9 //Adiciona o valor 9 a posição 4 do array/vetor.
+console.log(num)
+num.sort() //Organiza os elementos em ordem crescente.
+console.log(num)
+console.log(num.length)
 */
-
 
 
