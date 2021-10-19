d45115b68be8920419d3b73ec17a6ab1869d0ded
jonesramos01 <jonjones.rj@gmail.com>
2021-10-19 09:04:48 -0300

    commit

---
+++ JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/2.FUNÇÕES/script.js
@@ -0,0 +1,43 @@
+/*MÓDULO F: FUNÇÕES:*/
+/*
+//EX1:
+function parimpar(n){
+    if(n % 2 == 0){
+        return "Par!"
+    } else {
+        return "Impar!"
+    }
+}
+var res = parimpar(3) //A chamada deve ser colocada dentro de uma variável!
+console.log(res) //Impressão da chamada
+
+//
+n = Parâmetro
+Bloco if else = Ação
+parimpar() = Chamada
+"Par", "Impar" = Retorno
+
+
+//
+function soma(n1, n2){
+    return n1 + n2
+}
+console.log(soma(2, 5)) //Chamada = verificação/testes
+
+//Ou
+
+//Parâmetros não pré-definidos:
+function soma(n1, n2){ 
+    return n1 + n2
+}
+console.log(soma(7)) //Chamada = verificação/testes
+
+
+//Parâmetros pré-definidos:
+function soma(n1=0, n2=0){ //Parâmetros já pré definidos como zero para se caso na chamada não colocar outro valor ou tiver apenas 1 valor. Se sim, o outro valor passa a ser 0.
+    return n1 + n2
+}
+console.log(soma(7))
+
+OBS: Continuar o estudo sobre funções. Pois tem muitas outras sintaxes e formas de usar.
+*/
