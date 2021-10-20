15874a385d76d693741fbcf44a50b1dabf2a25a1
jonesramos01 <jonjones.rj@gmail.com>
2021-10-20 08:49:38 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/EX:/script.js
+++ JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/EX:/script.js
@@ -1,2 +1,24 @@
 /*EX: ANALISADOR DE NÚMEROS*/
-//
+/*Obs: 
+· Criar funções para os botões
+· Criar variáveis para as id's
+*/
+let num = document.getElementById("fnum")
+let lista = document.getElementById("flista")
+let res = document.getElementById("res")
+let valores = [] //Array declarado para análise de dados
+
+
+
+//1
+function adicionar(){
+    if(num >=1 && num <= 100){
+
+    } else {
+        window.alert("Valor inválido ou já encontrado na lista!")
+    }
+}
+
+function finalizar(){
+
+}
