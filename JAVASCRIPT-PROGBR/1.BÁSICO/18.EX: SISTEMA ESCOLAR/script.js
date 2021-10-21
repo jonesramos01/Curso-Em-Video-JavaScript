933f12d45d6b6ef4999c39b3b67a52e54616c658
jonesramos01 <jonjones.rj@gmail.com>
2021-10-21 10:07:24 -0300

    commit

--- JAVASCRIPT-PROGBR/1.BÁSICO/18.EX: SISTEMA ESCOLAR/script.js
+++ JAVASCRIPT-PROGBR/1.BÁSICO/18.EX: SISTEMA ESCOLAR/script.js
@@ -15,12 +15,19 @@ function media(n1, n2){
 }
 
 //3º Criação da lista:
+//A estrutura de repetição faz todo içamento dos termos anteriores. Ou seja, media(n1, n2) = media(notasA[index], notasA[index]). Mas se quiser declarar n1 e n2 para melhor entendimento, também pode.
 for(var index in nomes){
+
     //console.log(index) //Para ver o índice das posições
     //console.log(nomes[index]) //Para ver o índice dos nomes
     //console.log(notasA[index]) //Para ver o índice das notasA
     //console.log(notasB[index]) //Para ver o índice das notasB
-    console.log(nomes[index] + "-" + notasA[index] + "-" + notasB[index])
+    
+    //Declarando n1 e n2:
+    //var n1 = notasA[index];
+    //var n2 = notasB[index];
+
+    console.log(nomes[index] + "-" + notasA[index] + "-" + notasB[index] + "-" + media(notasA[index], notasB[index])) 
 }
 
 
