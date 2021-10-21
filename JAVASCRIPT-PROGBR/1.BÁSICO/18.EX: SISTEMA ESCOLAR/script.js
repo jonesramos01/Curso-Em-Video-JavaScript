b8258a32b27c79c4f4e9972ff408b533e34d8708
jonesramos01 <jonjones.rj@gmail.com>
2021-10-21 16:20:20 -0300

    commit

--- JAVASCRIPT-PROGBR/1.BÁSICO/18.EX: SISTEMA ESCOLAR/script.js
+++ JAVASCRIPT-PROGBR/1.BÁSICO/18.EX: SISTEMA ESCOLAR/script.js
@@ -4,19 +4,33 @@
 //APROVADO, REPROVADO
 //Obs: Em um sistema real todos esses dados estariam em um banco de dados, porém é possível simular essa atividade com Arrays.
 
-//1º Arrays simulando um banco de dados:
-var nomes = ["Jones", "Derek", "Thais"];
-var notasA = [7.0, 7.5, 8.0];
-var notasB = [7.0, 7.5, 8.0];
+//1º) Arrays simulando um banco de dados:
 
-//2º Função para calcular a média:
+var nomes = ["Jones", "Derek", "Thais"];
+var notasA = [7.0, 7.5, 5.0];
+var notasB = [7.0, 7.5, 5.0];
+
+
+//2º) Função para calcular a Média:
+
 function media(n1, n2){
-    return (n1 + n2)/2
+    return (n1 + n2)/2;
 }
 
-//3º Criação da lista:
+//4º) Função para os Aprovados ou Reprovados:
+
+function aprovado(media){
+    if(media >= 7){
+        return "Aprovado";
+    } else {
+        return "Reprovado";
+    }
+}
+
+//3º) Criação da lista:
 //A estrutura de repetição faz todo içamento dos termos anteriores. Ou seja, media(n1, n2) = media(notasA[index], notasA[index]). Mas se quiser declarar n1 e n2 para melhor entendimento, também pode.
-for(var index in nomes){
+
+for(var index in nomes){ //Verificar outras formas de fazer
 
     //console.log(index) //Para ver o índice das posições
     //console.log(nomes[index]) //Para ver o índice dos nomes
@@ -24,10 +38,11 @@ for(var index in nomes){
     //console.log(notasB[index]) //Para ver o índice das notasB
     
     //Declarando n1 e n2:
-    //var n1 = notasA[index];
-    //var n2 = notasB[index];
+    var n1 = notasA[index];
+    var n2 = notasB[index];
+    var m = media(n1, n2)
 
-    console.log(nomes[index] + "-" + notasA[index] + "-" + notasB[index] + "-" + media(notasA[index], notasB[index])) 
+    console.log(nomes[index] + " - " + n1 + " - " + n2 + " - " + m + " - " + aprovado(m)); 
 }
 
 
