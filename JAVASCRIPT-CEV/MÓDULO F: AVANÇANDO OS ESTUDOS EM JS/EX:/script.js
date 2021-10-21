a17f901911cc81d4e49def34aaa5d72393dfbf8b
Jones Ramos <jonjones.rj@gmail.com>
2021-10-21 07:43:43 -0300

    commit

--- JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/EX:/script.js
+++ JAVASCRIPT-CEV/MÓDULO F: AVANÇANDO OS ESTUDOS EM JS/EX:/script.js
@@ -8,12 +8,29 @@ let lista = document.getElementById("flista")
 let res = document.getElementById("res")
 let valores = [] //Array declarado para análise de dados
 
+//2º Adição dos elementos:
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
 
 
-//1
+//1º
 function adicionar(){
     if(num >=1 && num <= 100){
-
+        window.alert("Tudo ok!")
     } else {
         window.alert("Valor inválido ou já encontrado na lista!")
     }
