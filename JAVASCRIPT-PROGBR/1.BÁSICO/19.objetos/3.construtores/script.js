a662c92ca57df3ff7c5f4754073957e3d6dec76c
jonesramos01 <jonjones.rj@gmail.com>
2021-10-23 08:58:37 -0300

    commit

--- JAVASCRIPT-PROGBR/1.BÁSICO/19.objetos/3.construtores/script.js
+++ JAVASCRIPT-PROGBR/1.BÁSICO/19.objetos/3.construtores/script.js
@@ -1,9 +1,32 @@
 /*OBJETOS: CONSTRUTORES:*/
 //UM ARRAY TAMBÉM É UM OBJETO
+//....................................................................................................
+//1)
+/*
+function criarAluno(){
+    return{
+        nome: "Pedro",
+        nota1: 7.0,
+        nota2: 8.0,
+        media: function(){
+            return(this.nota1 + this.nota2)/2;
+        }
+    }
+}
+
+//TESTE
+var aluno = criarAluno();
+
+console.log(aluno);
+console.log(aluno.media());
+*/
+
+//....................................................................................................
+//2)
+/*
 var calcMedia = function(){
     return(this.nota1 + this.nota2)/2;
 }
-
 
 //1º) DECLARAÇÃO DOS DADOS:
 var turma = [ //ARRAY
@@ -12,13 +35,26 @@ var turma = [ //ARRAY
         nota1: 7.0,
         nota2: 8.0,
         media: calcMedia,
+    },
+
+    { //OBJETO
+        nome: "André",
+        nota1: 8.0,
+        nota2: 9.0,
+        media: calcMedia,
     }
+    
 ]
 
 //TESTE:
-var aluno = turma[0];
+
+var aluno = turma[0]; //Pedro
 console.log(aluno);
 console.log(aluno.media());
 
-
+var aluno = turma[1]; //João
+console.log(aluno);
+console.log(aluno.media());
+*/
+//....................................................................................................
 
