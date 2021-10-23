22dd20580c554d534ae5956befea028e9c45dd0b
Jones Ramos <jonjones.rj@gmail.com>
2021-10-22 21:17:52 -0300

    commit

--- JAVASCRIPT-PROGBR/1.BÁSICO/19.objetos/3.construtores/script.js
+++ JAVASCRIPT-PROGBR/1.BÁSICO/19.objetos/3.construtores/script.js
@@ -1,7 +1,13 @@
 /*OBJETOS: CONSTRUTORES:*/
+//UM ARRAY TAMBÉM É UM OBJETO
+var calcMedia = function(){
+    return(this.nota1 + this.nota2)/2;
+}
 
-var turma = [
-    {
+
+//1º) DECLARAÇÃO DOS DADOS:
+var turma = [ //ARRAY
+    { //OBJETO
         nome: "Pedro",
         nota1: 7.0,
         nota2: 8.0,
@@ -9,8 +15,10 @@ var turma = [
     }
 ]
 
-var calcMedia = function(){
-    return(this.nota1 + this.nota2)/2;
-}
+//TESTE:
+var aluno = turma[0];
+console.log(aluno);
+console.log(aluno.media());
 
-console.log(turma)
+
+
