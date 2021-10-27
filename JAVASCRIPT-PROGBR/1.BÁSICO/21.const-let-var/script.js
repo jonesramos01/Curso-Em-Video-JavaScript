a66d5f173f4092a1c7647ff06c04adbbd8b41189
jonesramos01 <jonjones.rj@gmail.com>
2021-10-27 07:43:13 -0300

    commit

---
+++ JAVASCRIPT-PROGBR/1.BÁSICO/21.const-let-var/script.js
@@ -0,0 +1,15 @@
+/*VAR, LET, CONST*/
+
+var num1 = 4;
+let num2 = 5;
+const num3 = 6;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+/*
+var → pode ser reatribuída (global - acessada de qualquer lugar do código)
+let → pode ser reatribuida (local: funciona somente a nível de bloco aonde a mesma foi criada)
+const → pode ser modificada (conteúdo), mas não pode ser reatribuida (valor)
+*/
