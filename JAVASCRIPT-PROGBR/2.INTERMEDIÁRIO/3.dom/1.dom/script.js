6f05079386c1bb84e7a3688673a0fd09cca1dd1b
jonesramos01 <jonjones.rj@gmail.com>
2021-10-27 16:34:53 -0300

    commit

---
+++ JAVASCRIPT-PROGBR/2.INTERMEDIÁRIO/3.dom/1.dom/script.js
@@ -0,0 +1,10 @@
+/*DOM:*/
+//TESTAR DIRETO NO NAVEGADOR:
+/*
+//document //Mostra o conteúdo do documento HTML (index.html)
+//document.body //Mostra o conteúdo de body, do documento HTML
+//document.body.innerHTML //Abre a possibilidade de modificar o documento diretamente do JS (navegador)
+Ex:
+document.body.innerHTML = "<h1>Este título foi inserido no JS</h1>" //Testar direto no navegador
+
+*/
