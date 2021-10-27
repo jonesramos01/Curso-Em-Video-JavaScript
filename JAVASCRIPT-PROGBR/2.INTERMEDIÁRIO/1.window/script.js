3474b33dd158ece92dbb15ab80a787f7341dd8a9
jonesramos01 <jonjones.rj@gmail.com>
2021-10-27 08:43:15 -0300

    commit

---
+++ JAVASCRIPT-PROGBR/2.INTERMEDIÁRIO/1.window/script.js
@@ -0,0 +1,13 @@
+/*WINDOW:*/
+//window.metodosxpto ... ()
+
+/**/
+//
+//window.console.log("Teste"); //Teste feito no navegador!
+
+//console.log("Teste");
+
+//
+//window.alert("Mensagem para o usuário!") //Teste feito no navegador!
+//Ou:
+//alert("Mensagem para o usuário!")
