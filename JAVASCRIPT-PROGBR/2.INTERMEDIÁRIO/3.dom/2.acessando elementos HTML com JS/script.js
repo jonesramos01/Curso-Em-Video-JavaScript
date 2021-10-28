6d97276ef7875ec77e575c369a4a19abbaaacfb0
jonesramos01 <jonjones.rj@gmail.com>
2021-10-28 08:21:13 -0300

    commit

--- JAVASCRIPT-PROGBR/2.INTERMEDIÁRIO/3.dom/2.acessando elementos HTML com JS/script.js
+++ JAVASCRIPT-PROGBR/2.INTERMEDIÁRIO/3.dom/2.acessando elementos HTML com JS/script.js
@@ -1,4 +1,6 @@
 /*ACESSANDO ELEMENTOS HTML COM JS:*/
+/*
+//1) Árvore DOM:
 
 document
 
@@ -7,14 +9,55 @@ document.head.title
 
 document.body
 
-/**/
-//ACESSAR UM ELEMENTO PELO ID:
+
+//2) Método para acessar qualquer elemento:
+
+//ACESSAR ELEMENTO PELO ID:
 document.getElementById()
 
-//ACESSAR UM ELEMENTO PELA CLASSE:
+//ACESSAR ELEMENTO PELA CLASSE (RETORNA UM ARRAY COM TODOS OS ELEMENTOS DA CLASSE):
 document.getElementsByClassName()
 
-//ACESSAR UM ELEMENTO PELO SELETOR:
+//ACESSAR ELEMENTO PELA TAG (RETORNA UM ARRAY COM TODOS OS ELEMENTOS DA TAG):
+document.getElementsByTagName
+
+//ACESSAR ELEMENTO PELO SELETOR:
 document.querySelector()
 
-//ACESSAR UM ELEMENTO PELA TAG:
+//ÁREA PARA TESTES DOM:
+
+//ID:
+//Apenas mostra o conteúdo
+document.getElementById("p1"); 
+
+//Ou:
+
+//Além de mostrar o conteúdo, o mesmo é anexado à uma variável:
+let p1 = document.getElementById("p1"); //Em seguida:
+console.log(p1);
+
+//CLASS:
+
+//Apenas mostra o conteúdo
+document.getElementsByClassName("paragrafo"); 
+
+//Ou:
+
+//Além de mostrar o conteúdo, o mesmo é anexado à uma variável:
+let paragrafo = document.getElementsByClassName("paragrafo"); //Em seguida:
+console.log(paragrafo);
+*/
+
+//TAG:
+
+//Apenas mostra o conteúdo:
+//document.getElementsByTagName("h1") //Apenas mostra o conteúdo
+
+//Ou:
+
+//Além de mostrar o conteúdo, o mesmo é anexado à uma variável:
+//let h1 = document.getElementsByTagName("h1"); //Em seguida:
+//console.log(h1);
+
+//OBS:
+//console.clear() //Para limpar o console
