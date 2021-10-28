5c435f72562d6d60c3a21af3565f5b00e44e3256
jonesramos01 <jonjones.rj@gmail.com>
2021-10-27 21:00:10 -0300

    commit

---
+++ JAVASCRIPT-PROGBR/2.INTERMEDIÁRIO/3.dom/2.acessando elementos HTML com JS/script.js
@@ -0,0 +1,20 @@
+/*ACESSANDO ELEMENTOS HTML COM JS:*/
+
+document
+
+document.head
+document.head.title
+
+document.body
+
+/**/
+//ACESSAR UM ELEMENTO PELO ID:
+document.getElementById()
+
+//ACESSAR UM ELEMENTO PELA CLASSE:
+document.getElementsByClassName()
+
+//ACESSAR UM ELEMENTO PELO SELETOR:
+document.querySelector()
+
+//ACESSAR UM ELEMENTO PELA TAG:
