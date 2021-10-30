13784208f6ec911f6325c9c05845d23817212c59
jonesramos01 <jonjones.rj@gmail.com>
2021-10-30 17:00:06 -0300

    commit

--- JAVASCRIPT-PROGBR/2.INTERMEDIÁRIO/3.dom/4.eventos/parte 2/script.js
+++ JAVASCRIPT-PROGBR/2.INTERMEDIÁRIO/3.dom/4.eventos/parte 2/script.js
@@ -1,26 +1,33 @@
 /*EVENTOS DOM:*/
 /*PARTE 1:*/
 /*··································································································*/
-//onclick:
+//ONCLICK: "FUNÇÃO DE AÇÃO AO CLICAR NA ÁREA DO MOUSE"
+//ONCHANGE:
 /*··································································································*/
 
 /*
 function clickH2(){
     console.log("Executando a função 'clickH2()' ao clicar em 'Clique aqui'")
 }
-*/
 
-/*··································································································*/
-//onchange:
-/*··································································································*/
-/*
 function changeH1(){
     let h1 = document.getElementById("clickH1")
     console.log(h1)
 }
 */
-/*PARTE 2:*/
 
+/*PARTE 2: EVENTOS DE MOUSE(BOTÃO)*/
+/*··································································································*/
+//ONMOUSEOVER: "AO ENTRAR COM O MOUSE NA ÁREA DO BOTÃO"
+//ONMOUSEOUT: "AO SAIR COM O MOUSE NA ÁREA DO BOTÃO"
+/*··································································································*/
 
+function onOver(elemento){
+    elemento.style.backgroundColor = "red";
+}
+
+function onOut(elemento){
+    elemento.style.backgroundColor = "orange";
+}
 
 /*PARTE 3:*/
